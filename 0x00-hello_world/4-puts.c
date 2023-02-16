e472dc27b8fea1c89f1a00ec95c83a2474ee876a
MelodyMulei <melodymmulei@gmail.com>
2023-02-16 12:34:54 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-
 /**
- * main . prints "Programming is like building a multilingual puzzle, followed by a new line"
- * return 0.
+ * main - Entry point
+ * Return: Always 0(Success)
  */
-
 int main(void)
 
 {
