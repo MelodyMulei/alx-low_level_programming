7c064cb38d01afb5116791d773167681d60e06fc
MelodyMulei <melodymmulei@gmail.com>
2023-04-03 08:07:40 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -18,33 +18,3 @@ size_t listint_len(const listint_t *h)
 
  return (num);
 }
-
-
-=============================
-
-2-add_nodeint.c
-
-#include "lists.h"
-
-/**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
- *
- * Return: pointer to the new node, or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
- listint_t *new;
-
- new = malloc(sizeof(listint_t));
- if (!new)
- return (NULL);
-
- new->n = n;
- new->next = *head;
- *head = new;
-
- return (new);
-}
-
